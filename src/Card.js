c9f38a8338322671a1eefdffe525c874a138c20e
Ivan Zbyrnia <97780599+bazinygero@users.noreply.github.com>
2023-02-18 12:37:58 +0200

    svg import, add styling

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import locationIcon from "./assets/location-icon.svg"
+ 
 export default function Card(props) {
     return(
+        <>
         <div className="card">
             <div className="card__img">
                 <img src={props.cardImg} alt="card image" />
@@ -9,18 +11,20 @@ export default function Card(props) {
             <div className="card__info">
                 <div className="card__info-location">
                     <div className="card__info-location-name">
-                        <img src="/assets/location-icon.svg" alt="location icon" />
+                        <img src={locationIcon} alt="location icon" />
                         <h4>{props.location}</h4>
                     </div>
-                    <div className="card_info-location-link">
+                    <div className="card__info-location-link">
                         <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8">View on Google Maps</a>
                     </div>
                 </div>
                 <h2>{props.title}</h2>
-                <span>{props.startDate} - {props.endDate}</span>
-                <p>{props.description}</p>
+                <span className="dates">{props.startDate} - {props.endDate}</span>
+                <p className="para">{props.description}</p>
             </div>
-            <hr />
         </div>
+        <hr />
+        </>
+
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import logo from "./assets/logo.svg"
 
 export default function Navbar() {
     return(
         <nav>
-            <img src="/assets/logo.svg" alt="logo" />
+            <img src={logo} alt="logo" />
             <h3>my travel journal.</h3>
         </nav> 
     )
